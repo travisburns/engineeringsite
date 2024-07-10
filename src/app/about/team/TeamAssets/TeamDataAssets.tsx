56e798e028b31025f5669fe08d50f1b915c6449c
travisburns <travisjb0@gmail.com>
2024-07-10 14:40:43 -0700

    Fixed production errors 2

--- src/app/about/team/TeamAssets/TeamDataAssets.tsx
+++ src/app/about/team/TeamAssets/TeamDataAssets.tsx
@@ -1,10 +1,10 @@
 // team/TeamAssets/TeamDataAssets.ts
-import Greg from './gregg.jpg'
+import Greg from './Gregg.jpg'
 import Edward from './EdCarlisle.png'
 import MarkW from './MarkW.jpg'
 import Jeffrey from './jeffHoward.png'
 import MarkJ from './MarkJ.jpg'
-import Jonathan from './Jonathantest.jpg'
+import Jonathan from './jonathantest.jpg'
 import Sam from './SamR.jpg'
 
 export { Greg, Edward, MarkW, Jeffrey, MarkJ, Jonathan, Sam }
